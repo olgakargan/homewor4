b6e66763daeff556f9f2107b33dbfdc6aef4a353
Olga Kargan <olgakargan72@gmail.com>
2022-11-05 16:17:52 +0400

    Initial commit

---
+++ src/main/java/com/example/homewor4/Homewor4Application.java
@@ -0,0 +1,13 @@
+package com.example.homewor4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homewor4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homewor4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homewor4/Homewor4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homewor4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homewor4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
